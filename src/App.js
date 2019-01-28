52f312478738c38a0423ebf805d887b463d4eaf7
louis <clouchtibat@gmail.com>
2019-01-29 00:08:04 +0100

    WIP adding plants

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
 
-import plants from './plants';
+import originalPlants from './plants';
 
 import './app.scss';
 
@@ -22,34 +22,92 @@ const months = [
 
 const unit = Math.floor(100 / 12);
 
+const sortFunctions = {
+	name: asc => (a, b) => {
+		let result = 0;
+		if (a.name < b.name) {
+			result = -1;
+		}
+		if (a.name > b.name) {
+			result = 1;
+		}
+		return result * (asc ? -1 : 1);
+	},
+	date: (period, which, asc) => (a, b) => {
+		const periodA = a.periods[period] || [-1, -1];
+		const periodB = b.periods[period] || [-1, -1];
+		return (periodA[which] - periodB[which]) * (asc ? -1 : 1);
+	}
+};
+
+const sortMapper = (value, asc) => {
+	const [key, which] = value.split('_');
+	console.log('sortMapper');
+	console.log(key);
+	console.log(which);
+	switch (key) {
+		case 'semis':
+		case 'semisDirect':
+		case 'repiquage':
+		case 'plantage':
+		case 'recolte':
+			return sortFunctions.date(key, parseInt(which, 10), asc);
+		default:
+			return sortFunctions.name(asc);
+	}
+};
+
 class App extends Component {
 	constructor() {
 		super();
 		this.state = {
-			plants: plants.slice().sort((a, b) => a.name - b.name)
+			sort: 'name',
+			asc: false
 		};
 	}
 
 	onSort = e => {
 		console.log('on sort');
 		console.log(e.target.value);
+		this.setState({ sort: e.target.value });
+	};
+
+	onChangeOrder = () => {
+		console.log('on change order');
+		this.setState({ asc: !this.state.asc });
 	};
 
 	render() {
-		const { onSort } = this;
-		const { plants } = this.state;
+		const { onSort, onChangeOrder } = this;
+		const { sort, asc } = this.state;
+		console.log('render', sort, asc);
 		return (
 			<div className="app">
 				<div className="header">
 					<select onChange={onSort}>
 						<option value="name">Nom</option>
-						<option value="start">Début</option>
-						<option value="end">Fin</option>
+						<optgroup label="Semis">
+							<option value="semis_0">Début</option>
+							<option value="semis_1">Fin</option>
+						</optgroup>
+						<optgroup label="Semis direct">
+							<option value="semisDirect_0">Début</option>
+							<option value="semisDirect_1">Fin</option>
+						</optgroup>
+						<optgroup label="Plantage">
+							<option value="plantage_0">Début</option>
+							<option value="plantage_1">Fin</option>
+						</optgroup>
+						<optgroup label="Récolte">
+							<option value="recolte_0">Début</option>
+							<option value="recolte_1">Fin</option>
+						</optgroup>
 					</select>
+					<button onClick={onChangeOrder}>order</button>
 				</div>
 				<div className="chart">
 					<div className="plants">
-						{plants.map(({ id, name, periods }, index) => (
+						{[...originalPlants].sort(sortMapper(sort, asc)).map(({ id, name, periods }, index) => (
 							<div key={id} className="plant">
 								<div className="name" title={name}>
 									{name}
@@ -57,6 +115,7 @@ class App extends Component {
 								<div className="periods">
 									{Object.keys(periods).map(key => (
 										<div
+											key={key}
 											className={classNames('period', key.toLowerCase())}
 											style={{
 												width: unit * (periods[key][1] - periods[key][0]) + '%',
--- src/plants.js
+++ src/plants.js
@@ -1,11 +1,165 @@
 export default [
 	{
-		id: 'id1',
-		name: 'nom de plante de nom de plante de nom de plante',
+		id: 'a1',
+		name: 'Chou Frisé Westlandse Winter',
 		periods: {
-			semis: [1, 3],
-			semisDirect: [4, 6],
-			repiquage: [7, 9]
+			semis: [3.5, 5],
+			repiquage: [4.5, 5],
+			plantage: [6.9, 7],
+			recolte: [10.9, 12]
+		}
+	},
+	{
+		id: 'a2',
+		name: 'Radis Rond Ecarlate',
+		periods: {
+			semisDirect: [0, 8]
+		}
+	},
+	{
+		id: 'a3',
+		name: "Laitue d'Eté Kagraner Sommer 2",
+		periods: {
+			semisDirect: [2, 6]
+		}
+	},
+	{
+		id: 'a4',
+		name: 'Chou-Rave Blanc Géant Superschmelz',
+		periods: {
+			semis: [0, 3],
+			semisDirect: [3, 6]
+		}
+	},
+	{
+		id: 'a5',
+		name: 'Cardon Plein Blanc Inerme',
+		periods: {
+			semisDirect: [4, 5],
+			recolte: [0, 11]
+		}
+	},
+	{
+		id: 'a6',
+		name: 'Chicorée Scarole Géante Maraichère',
+		periods: {
+			semisDirect: [5, 7.25],
+			recolte: [8.1, 12]
+		}
+	},
+	{
+		id: 'a7',
+		name: 'Laitue de printemps Appia',
+		periods: {
+			semis: [1, 2],
+			plantage: [3, 4],
+			semisDirect: [3, 4]
+		}
+	},
+	{
+		id: 'a8',
+		name: 'Carotte Touchon',
+		periods: {
+			semisDirect: [2, 6]
+		}
+	},
+	{
+		id: 'a9',
+		name: 'Courge Musquée Muscade',
+		periods: {
+			semis: [2, 3],
+			plantage: [4, 6],
+			semisDirect: [4.5, 5],
+			recolte: [8.5, 10]
+		}
+	},
+	{
+		id: 'a10',
+		name: 'Tomate Poire Jaune',
+		periods: {
+			semis: [2, 3.3],
+			repiquage: [2.5, 3.8],
+			plantage: [4.5, 4.8]
+		}
+	},
+	{
+		id: 'a11',
+		name: 'Tomate Cerise Noire',
+		periods: {
+			semis: [2, 3.3],
+			repiquage: [2.5, 3.8],
+			plantage: [4.5, 4.8]
+		}
+	},
+	{
+		id: 'a12',
+		name: 'Epinard Matador Viking',
+		periods: {
+			semisDirect: [1, 2],
+			recolte: [3, 7]
+		}
+	},
+	{
+		id: 'a13',
+		name: 'Chicorée Pain de Sucre',
+		periods: {
+			semisDirect: [5.7, 6.3],
+			recolte: [9.5, 11]
+		}
+	},
+	{
+		id: 'a14',
+		name: 'Navet Blanc Globe à Collet Violet',
+		periods: {
+			semisDirect: [7, 8.3],
+			recolte: [9.5, 11]
+		}
+	},
+	{
+		id: 'a15',
+		name: 'Chicorée Frisée Grosse Pancalière',
+		periods: {
+			semisDirect: [5, 7.2],
+			recolte: [9, 11]
+		}
+	},
+	{
+		id: 'a16',
+		name: 'Chou de Bruxelles de Rosny',
+		periods: {
+			semis: [3, 5],
+			recolte: [11.5, 12]
+		}
+	},
+	{
+		id: 'a17',
+		name: "Epinard Géant d'Hiver",
+		periods: {
+			semisDirect: [7, 8],
+			recolte: [8.9, 12]
+		}
+	},
+	{
+		id: 'a18',
+		name: 'Chou Cabus Amager Lav',
+		periods: {
+			semis: [3, 6],
+			recolte: [8.9, 12]
+		}
+	},
+	{
+		id: 'a19',
+		name: 'Claytone de Cuba',
+		periods: {
+			semisDirect: [3, 7],
+			recolte: [8, 12.2]
+		}
+	},
+	{
+		id: 'a20',
+		name: 'Cornichon Vert Fin de Meaux',
+		periods: {
+			semisDirect: [4, 5]
 		}
 	}
 ];
